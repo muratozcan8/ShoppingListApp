e154fa1a579bc019d7771a617a5d92f9c8b35227
Murat ozcan <mrtzcn08@gmail.com>
2024-03-11 23:26:07 +0300

    created dao and room database

---
+++ app/src/main/java/com/muratozcan/shoppinglistapp/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.muratozcan.shoppinglistapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+
+    }
+}
