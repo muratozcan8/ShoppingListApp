addd5dbc1b615bfd4bcc6695a73ffcd79175844b
Murat ozcan <mrtzcn08@gmail.com>
2024-03-13 16:32:38 +0300

    integrated kodein dependency injection

--- app/src/main/java/com/muratozcan/shoppinglistapp/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/muratozcan/shoppinglistapp/ui/shoppinglist/ShoppingActivity.kt
@@ -1,30 +1,30 @@
 package com.muratozcan.shoppinglistapp.ui.shoppinglist
 
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.muratozcan.shoppinglistapp.R
 import com.muratozcan.shoppinglistapp.data.db.ShoppingDatabase
 import com.muratozcan.shoppinglistapp.data.db.entities.ShoppingItem
 import com.muratozcan.shoppinglistapp.data.repositories.ShoppingRepository
 import com.muratozcan.shoppinglistapp.databinding.ActivityShoppingBinding
 import com.muratozcan.shoppinglistapp.other.ShoppingItemAdapter
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class ShoppingActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity(), KodeinAware {
 
     private lateinit var binding: ActivityShoppingBinding
+
+    override val kodein by kodein()
+    private val factory: ShoppingViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShoppingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val database = ShoppingDatabase(this)
-        val repository = ShoppingRepository(database)
-        val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
 
